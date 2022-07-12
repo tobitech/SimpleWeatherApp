bb88d829c6ac0cc2b952ffb96ec09f3bb530c653
Oluwatobi Omotayo <tobitech@ymail.com>
2022-07-12 13:32:28 +0100

    Initial Commit

---
+++ SimpleWeatherApp/SimpleWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWeatherAppApp.swift
+//  SimpleWeatherApp
+//
+//  Created by Oluwatobi Omotayo on 12/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
