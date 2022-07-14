51ad83901f8470b251070e274d8fa33c68731a92
Oluwatobi Omotayo <tobitech@ymail.com>
2022-07-14 07:09:23 +0100

    Create a wrapper around the NWPath model we couldn't initialise so that we can use it in our mock versions of the PathMonitorClient.
    Create two mocks that returns satisfied and unsatisfied network conditions.

--- WeatherFeature/ContentView.swift
+++ WeatherFeature/ContentView.swift
@@ -10,10 +10,30 @@ import Network
 import SwiftUI
 import WeatherClient
 
+
+// we are calling it NetworkPath in order to avoid conflict with other types called Path.
+// usually we will just drop the NW namespace for the type we want to mimic.
+// in this case that's not possible.
+public struct NetworkPath {
+  // we will add all the fields that we're interested in accessing from the NWPath.
+  public var status: NWPath.Status
+}
+
+extension NetworkPath {
+  // we need to add a conveniece initializer so that this is easy for us to work with.
+  // since status is an enum, we can make one that takes the raw value of the path
+  // since our `NetworkPath` is a wrapper it makes sense to make an initializer that takes NWPath.
+  public init(rawValue: NWPath) {
+    self.status = rawValue.status
+  }
+}
+
 // All we've done here is written a little struct wrapper
 // that exposes endpoints that mimic the endpoints that we would want to call on a path monitor.
 public struct PathMonitorClient {
-  public var setPathUpdateHandler: (@escaping (NWPath) -> Void) -> Void
+//  public var setPathUpdateHandler: (@escaping (NWPath) -> Void) -> Void
+  // now that we've a wrapper we can replace NWPath with that wrapper here.
+  public var setPathUpdateHandler: (@escaping (NetworkPath) -> Void) -> Void
   public var start: (DispatchQueue) -> Void
 }
 
@@ -22,10 +42,36 @@ extension PathMonitorClient {
   static var live: Self {
     let monitor = NWPathMonitor()
     return Self (
-      setPathUpdateHandler: { monitor.pathUpdateHandler = $0 },
+      setPathUpdateHandler: { callback in
+        monitor.pathUpdateHandler = { path in
+          callback(NetworkPath(rawValue: path))
+        }
+      },
       start: monitor.start(queue:)
     )
   }
+}
+
+extension PathMonitorClient {
+  // let's start with a mock that provides ideal internet connectivity.
+  static let satisfied = Self(
+    setPathUpdateHandler: { callback in
+      // NWPath doesn't have a way of constructing this value
+      // callback(/*satisified*/)
+      callback(NetworkPath(status: .satisfied))
+    },
+    // we don't need to worry about starting it here
+    // we can just start off from a satisfied state
+    // that's why we're passing in en empty closure.
+    start: { _ in }
+  )
+  
+  static let unsatisfied = Self(
+    setPathUpdateHandler: { callback in
+      callback(NetworkPath(status: .unsatisfied))
+    },
+    start: { _ in }
+  )
 }
 
 public class AppViewModel: ObservableObject {
@@ -57,7 +103,8 @@ public class AppViewModel: ObservableObject {
     }
     pathMonitorClient.start(.main)
     
-    self.refreshWeather()
+    // this was causing a bug - we are removing it so that we let the path update drive the refresh call.
+    // self.refreshWeather()
   }
   
   func refreshWeather() {
@@ -135,7 +182,7 @@ struct ContentView_Previews: PreviewProvider {
     
     return ContentView(
       viewModel: AppViewModel(
-        pathMonitorClient: .live,
+        pathMonitorClient: .satisfied,
         weatherClient: client
       )
     )
