379508b0b711954413f9e7b455041f171e0e4d28
Oluwatobi Omotayo <tobitech@ymail.com>
2022-07-16 09:54:48 +0100

    Implement a live implementation of the LocationClient wrapper interface that uses the actual CLLocationManager to request for location and provide status

--- WeatherFeature/ContentView.swift
+++ WeatherFeature/ContentView.swift
@@ -11,10 +11,10 @@ import SwiftUI
 import WeatherClient
 import PathMonitorClient
 
-struct LocationClient {
-  var authorizationStatus: () -> CLAuthorizationStatus
-  var requestWhenInUseAuthorization: () -> Void
-  var requestLocation: () -> Void
+public struct LocationClient {
+  public var authorizationStatus: () -> CLAuthorizationStatus
+  public var requestWhenInUseAuthorization: () -> Void
+  public var requestLocation: () -> Void
   
   // we could feed the view model through this closure to any live underlying manager.
   // while this is a great approach, it would not let us fully control the dependency.
@@ -22,9 +22,9 @@ struct LocationClient {
   // but we don't want our features viewmodel to know about any live framework or implementation.
   // var setDelegate: (CLLocationManagerDelegate) -> Void
   
-  var delegate: AnyPublisher<DelegateEvent, Never>
+  public var delegate: AnyPublisher<DelegateEvent, Never>
   
-  enum DelegateEvent {
+  public enum DelegateEvent {
     case didChangeAuthorization(CLAuthorizationStatus)
     case didUpdateLocations([CLLocation])
     case didFailWithError(Error)
@@ -32,7 +32,7 @@ struct LocationClient {
 }
 
 extension LocationClient {
-  static var live: Self  {
+  public static var live: Self  {
     
     // this is only needed for live implementation that's why we're creating this scoped type so that it's only accessible here.
     // also prevents namespace conflicts.
@@ -74,7 +74,7 @@ extension LocationClient {
   }
 }
 
-public class AppViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+public class AppViewModel: ObservableObject {
   @Published var currentLocation: Location?
   @Published var isConnected = true
   @Published var weatherResults: [WeatherResponse.ConsolidatedWeather] = []
@@ -82,28 +82,23 @@ public class AppViewModel: NSObject, ObservableObject, CLLocationManagerDelegate
   var weatherRequestCancellable: AnyCancellable?
   var pathUpdateCancellable: AnyCancellable?
   var searchLocationsCancellable: AnyCancellable?
+  var locationDelegateCancellable: AnyCancellable?
   
   let weatherClient: WeatherClient
   let pathMonitorClient: PathMonitorClient
-  
-  let locationManager = CLLocationManager()
+  let locationClient: LocationClient
   
   // we removed the .live default because we don't want the view model to know about a live client
   // so that it isn't always waiting for the module it leaves in to compile first.
   public init(
 //    isConnected: Bool = true,
+    locationClient: LocationClient,
     pathMonitorClient: PathMonitorClient,
     weatherClient: WeatherClient
   ) {
+    self.locationClient = locationClient
     self.weatherClient = weatherClient
     self.pathMonitorClient = pathMonitorClient
-//    let pathMonitor = NWPathMonitor()
-//    self.isConnected = isConnected
-    
-    // now handled by the publisher
-//    self.pathMonitorClient.setPathUpdateHandler { [weak self] path in
-    
-    super.init()
     
     self.pathUpdateCancellable = self.pathMonitorClient.networkPathPublisher
       .map { $0.status == .satisfied }
@@ -120,13 +115,47 @@ public class AppViewModel: NSObject, ObservableObject, CLLocationManagerDelegate
         }
       })
     
-    // now handled by the publisher
-    // self.pathMonitorClient.start(.main)
-    
-    // this was causing a bug - we are removing it so that we let the path update drive the refresh call.
-    // self.refreshWeather()
-    
-    self.locationManager.delegate = self
+    self.locationDelegateCancellable =  self.locationClient.delegate.sink { event in
+      switch event {
+      case let .didChangeAuthorization(status):
+        switch status {
+        case .notDetermined:
+          // not likely that we get this status after first interaction with location manager.
+          break
+          
+        case .restricted:
+          // TODO: Show an alert
+          // this will have a different alert message since it's likely it's intentional.
+          break
+        case .denied:
+          // TODO: Show an alert
+          // same as above.
+          break
+          
+        case .authorizedAlways, .authorizedWhenInUse, .authorized:
+          self.locationClient.requestLocation()
+          
+        @unknown default:
+          break
+        }
+        
+      case let .didUpdateLocations(locations):
+        guard let location = locations.first else { return }
+        
+        self.searchLocationsCancellable = self.weatherClient.searchLocations(location.coordinate).sink(
+          receiveCompletion: { _ in
+            // normally we will do an error handling and show alert here if this fails.
+          },
+          receiveValue: { [weak self] locations in
+            self?.currentLocation = locations.first
+            self?.refreshWeather()
+          }
+        )
+        
+      case .didFailWithError(_):
+        break
+      }
+    }
   }
   
   // now handled by the publisher
@@ -152,10 +181,10 @@ public class AppViewModel: NSObject, ObservableObject, CLLocationManagerDelegate
   func locationButtonTapped() {
     // the logic we want to put here is to first check if the user has previously given us access to their location.
     
-    switch CLLocationManager.authorizationStatus() {
+    switch self.locationClient.authorizationStatus() {
     case .notDetermined:
       // when we don't know the status - we should request for authorization.
-      self.locationManager.requestWhenInUseAuthorization()
+      self.locationClient.requestWhenInUseAuthorization()
     case .restricted:
       // restricted by parental control.
       // TODO: Show an alert
@@ -166,53 +195,11 @@ public class AppViewModel: NSObject, ObservableObject, CLLocationManagerDelegate
       break
     case .authorizedAlways, .authorizedWhenInUse:
       // whichever authorization type we were granted we can request the location data.
-      self.locationManager.requestLocation()
+      self.locationClient.requestLocation()
       
     @unknown default:
       break
     }
-  }
-  
-  // MARK: - CLLocationManagerDelegate
-  public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-    switch status {
-    case .notDetermined:
-      // not likely that we get this status after first interaction with location manager.
-      break
-
-    case .restricted:
-      // TODO: Show an alert
-      // this will have a different alert message since it's likely it's intentional.
-      break
-    case .denied:
-      // TODO: Show an alert
-      // same as above.
-      break
-      
-    case .authorizedAlways, .authorizedWhenInUse, .authorized:
-      self.locationManager.requestLocation()
-
-    @unknown default:
-      break
-    }
-  }
-  
-  public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    guard let location = locations.first else { return }
-    
-    self.searchLocationsCancellable = self.weatherClient.searchLocations(location.coordinate).sink(
-      receiveCompletion: { _ in
-        // normally we will do an error handling and show alert here if this fails.
-      },
-      receiveValue: { [weak self] locations in
-        self?.currentLocation = locations.first
-        self?.refreshWeather()
-      }
-    )
-  }
-  
-  public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-    // the app will just crash if there is an error and this delegate method wasn't called.
   }
 }
 
@@ -279,6 +266,7 @@ struct ContentView_Previews: PreviewProvider {
     
     return ContentView(
       viewModel: AppViewModel(
+        locationClient: .live,
         pathMonitorClient: .satisfied,
         weatherClient: client
       )
