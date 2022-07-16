7c1ddfa67dfbaeb901eac1e8959dbe291a09fe20
Oluwatobi Omotayo <tobitech@ymail.com>
2022-07-16 09:38:33 +0100

    hook up the delegate subject. the delegate is captured inside the live client and cleared out when subject is cancelled.
    this is because the location manager's delegate holds a weak reference.

--- WeatherFeature/ContentView.swift
+++ WeatherFeature/ContentView.swift
@@ -32,16 +32,44 @@ struct LocationClient {
 }
 
 extension LocationClient {
-  static var live: Self {
+  static var live: Self  {
+    
+    // this is only needed for live implementation that's why we're creating this scoped type so that it's only accessible here.
+    // also prevents namespace conflicts.
+    class Delegate: NSObject, CLLocationManagerDelegate {
+      
+      var subject: PassthroughSubject<DelegateEvent, Never>
+      
+      init(subject: PassthroughSubject<DelegateEvent, Never>) {
+        self.subject = subject
+        super.init()
+      }
+      
+      func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        self.subject.send(.didChangeAuthorization(status))
+      }
+      
+      func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.subject.send(.didUpdateLocations(locations))
+      }
+      
+      func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        self.subject.send(.didFailWithError(error))
+      }
+    }
     
     let locationManager = CLLocationManager()
     let subject = PassthroughSubject<DelegateEvent, Never>()
+    var delegate: Delegate? = Delegate(subject: subject)
+    locationManager.delegate = delegate
     
     return Self(
       authorizationStatus: CLLocationManager.authorizationStatus,
       requestWhenInUseAuthorization: locationManager.requestWhenInUseAuthorization,
       requestLocation: locationManager.requestLocation,
-      delegate: subject.eraseToAnyPublisher()
+      delegate: subject
+        .handleEvents(receiveCancel: { delegate = nil })
+        .eraseToAnyPublisher()
     )
   }
 }
