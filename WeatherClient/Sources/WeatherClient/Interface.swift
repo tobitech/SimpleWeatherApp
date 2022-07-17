c7bc352704b5284688e6dad945c92a2d039d52ff
Oluwatobi Omotayo <tobitech@ymail.com>
2022-07-17 06:28:14 +0100

    fix any issue preventing our test targets from building.
    for some basic tests write some assertions against the published variables on the view model.

--- WeatherClient/Sources/WeatherClient/Interface.swift
+++ WeatherClient/Sources/WeatherClient/Interface.swift
@@ -33,10 +33,36 @@ public struct WeatherResponse: Decodable, Equatable {
     public var maxTemp: Double
     public var minTemp: Double
     public var theTemp: Double
+    
+    public init(
+      applicableDate: Date,
+      id: Int,
+      maxTemp: Double,
+      minTemp: Double,
+      theTemp: Double
+    ) {
+      self.applicableDate = applicableDate
+      self.id = id
+      self.maxTemp = maxTemp
+      self.minTemp = minTemp
+      self.theTemp = theTemp
+    }
+  }
+  
+  public init(consolidatedWeather: [ConsolidatedWeather]) {
+    self.consolidatedWeather = consolidatedWeather
   }
 }
 
-public struct Location: Decodable {
+public struct Location: Equatable, Decodable {
   public var title: String
   public var woeid: Int
+  
+  public init(
+    title: String,
+    woeid: Int
+  ) {
+    self.title = title
+    self.woeid = woeid
+  }
 }
